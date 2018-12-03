98263f1e3e3ed4a7071dcf588ea363c45cf11134
Mark Pauley <pauley@unsaturated.net>
2018-12-02 22:47:59 -0800

    Day 3, Part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::BufReader;
 
 use regex::Regex;
@@ -17,32 +18,40 @@ fn main() -> io::Result<()> {
         let input = BufReader::new(file);
         let lines : Vec<String> = input.lines().map(|x|{x.unwrap()}).collect();
         let re = Regex::new(r"#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)").unwrap();
-        let mut taken = HashSet::<(i32,i32)>::new();
-        let mut dup = HashSet::<(i32,i32)>::new();
-        for line in &lines {
+        let mut taken = HashMap::new();
+        let mut dup = HashSet::new();
+
+        let mut clean = HashSet::new();
+        for line in &lines {            
             //println!("Reading line: {}", &line);
             if let Some(captures) = re.captures(&line) {
-                let coords : Vec<i32> = captures.iter().skip(2).map(|x|{ x.unwrap().as_str().parse::<i32>().unwrap() }).collect();
-                let (x_0, y_0, w, h) = (coords[0], coords[1], coords[2], coords[3]);
+                let coords : Vec<i32> = captures.iter().skip(1).map(|x|{ x.unwrap().as_str().parse::<i32>().unwrap() }).collect();
+                let (id, x_0, y_0, w, h) = (coords[0], coords[1], coords[2], coords[3], coords[4]);
                 //println!("Parsed as {},{} {}x{}", x_0, y_0, w, h);
+                clean.insert(id);
                 for x in x_0..(x_0 + w) {
                     for y in y_0..(y_0 + h) {
                         let point = (x, y);
                         //println!("({},{})", x,y);
-                        if taken.contains(&point) {                            
+                        if taken.contains_key(&point) {
                             dup.insert(point);
+                            clean.remove(&id);
+                            clean.remove(&taken[&point]);
                         }
                         else {
-                            taken.insert(point);
+                            taken.insert(point, id);
                         }
                     }
-                }
+                }                
             }
             else {
                 println!("Bad input \"{}\" didn't match!", &line);
             }
         }
         println!("Part1: {}", dup.len());
+        for id in clean {
+            println!("Part2: {}", id);
+        }
     }
     else {
         println!("Usage: {} <input_file>", prog_name.unwrap());
